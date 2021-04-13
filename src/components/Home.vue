8282e69632294d0bf730304cca3789cec0b0c438
waxijiang <1158612615@qq.com>
2021-04-13 15:35:44 +0800

    [新功能]:  伪队伍创建

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,7 +22,7 @@
                 <el-avatar
                   shape="square"
                   :size="50"
-                  :src="squareUrl"
+                  src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
                 ></el-avatar>
                 <div class="notify-item-body">
                   <div class="notify-item-name">黄希希</div>
