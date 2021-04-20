66017dac59cf6412271d219b68cbd6f6a1f801d9
wjh <2586549284@qq.com>
2021-04-20 15:14:23 +0800

    新手指引

--- src/components/Unaction.vue
+++ src/components/Unaction.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="action-list">
+    <div class="action-list" data-step="4" data-intro="点击活动 选择发送区域">
       <ActionCard
         v-for="action in unactionList"
         :key="action.id"
@@ -11,6 +11,9 @@
   </div>
 </template>
 <script>
+// 导出组件数据
+import introJs from "intro.js";
+import "intro.js/introjs.css";
 import ActionCard from "@/components/ActionCard";
 export default {
   components: {
@@ -42,6 +45,22 @@ export default {
     },
   },
   methods: {
+    guide2() {
+      introJs()
+        .setOptions({
+          nextLabel: "下一个", // 下一个按钮文字
+          prevLabel: "上一个", // 上一个按钮文字
+          skipLabel: "跳过", // 跳过按钮文字
+          doneLabel: "立即体验", // 完成按钮文字
+          hidePrev: true, // 在第一步中是否隐藏上一个按钮
+          hideNext: true, // 在最后一步中是否隐藏下一个按钮
+          exitOnOverlayClick: false, // 点击叠加层时是否退出介绍
+          showStepNumbers: false, // 是否显示红色圆圈的步骤编号
+          disableInteraction: true, // 是否禁用与突出显示的框内的元素的交互，就是禁止点击
+          showBullets: false, // 是否显示面板指示点
+        })
+        .start();
+    },
     getunactionlist() {
       this.$http({
         method: "get",
@@ -55,6 +74,9 @@ export default {
   },
   created() {
     this.getunactionlist();
+  },
+  mounted() {
+    this.guide2();
   },
 };
 </script>
